bed0d24e45a942bc37764bd54baa3ed2c25ffeb5
conrad96 <conrad.mugisha@gmail.com>
2019-12-01 12:38:01 +0300

    added login validating code

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -9,9 +9,25 @@ class Login extends CI_Controller {
        $this->load->view("login/index");       
     }
     function login(){
+        $data = array();
         if(!empty($_POST)){
-
+            $username = $_POST['username'];
+            $password = md5($_POST['password']);
+            $fetch_user = $this->db->query("SELECT * FROM users u WHERE u.uname LIKE '".$username."' AND u.password LIKE '".$password."'")->result();
+            if(!empty($fetch_user)){
+                //set session
+                $userdata = array();
+                foreach($fetch_user as $user){
+                    $userdata['userid'] = $user->id;
+                    $userdata['names'] = $user->full_names;
+                    $userdata['role'] = $user->role;
+                }
+                $this->session->set_userdata($userdata);
+            }else{
+                $data['msg'] = "incorrect username or password";
+            }
         }
+        $this->load->view("login/index", $data);
     }
 
 }
