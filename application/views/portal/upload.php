b40783937ea4cf6d57c939a3f89e8b75d39b6a57
conrad96 <conrad.mugisha@gmail.com>
2019-12-01 22:22:54 +0300

    .

--- application/views/portal/upload.php
+++ application/views/portal/upload.php
@@ -27,10 +27,7 @@
           </div>
         </div>   
         <div class="row">
-        <?php 
-        print '<pre>';
-        print_r();
-        ?>
+        
         </div>   
 
       </section>
