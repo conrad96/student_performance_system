848c35e445c2cc6e7c7600c11a831e20a2f7bd7d
conrad96 <conrad.mugisha@gmail.com>
2019-12-01 12:32:11 +0300

    extra tweaks on config

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,12 +1,17 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Login extends CI_Controller {
 
     function __construct(){
         parent::__construct();
     }
     function index(){
-        $this->load->view("login/index");
+       $this->load->view("login/index");       
+    }
+    function login(){
+        if(!empty($_POST)){
+
+        }
     }
 
 }
