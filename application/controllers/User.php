e64cb2e5ab74274b39875e25a2c9178242176a9a
conrad96 <conrad.mugisha@gmail.com>
2019-12-12 10:22:43 +0300

    extract all students records and save into database

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -13,137 +13,134 @@ class User extends CI_Controller {
         $this->load->view("portal/index", $data);
     }
     function upload(){
-        $data['page_title'] = 'Upload';
+        $data['page_title'] = 'Upload';      
         if(!empty($_POST)){
-            //process file uploaded
-            //move file to samples folder
-            $file_name = $_FILES['recordFile']['name'];
-            $file = $_FILES['recordFile']['tmp_name'];
-            $sample_name = strtolower(str_replace(' ', '_', $_POST['record_file']));
+          //process file uploaded
+          //move file to samples folder
+          $file_name = $_FILES['recordFile']['name'];
+          $file = $_FILES['recordFile']['tmp_name'];
+          $sample_name = strtolower(str_replace(' ', '_', $_POST['record_file']));
 
-            $path = $_SERVER['DOCUMENT_ROOT'];
-            $fileName = $sample_name.'_'.$file_name;
-            if(move_uploaded_file($file, $path.'/sps/assets/samples/'.$fileName)){
-                //store in database
-                $sample = array();
-                $sample['sample_name'] = $_POST['record_file'];
-                $sample['sample_file'] = $fileName;
-                $sample['author'] = $this->session->userdata['userid'];
-                $this->db->insert("sample_data", $sample);
-                //store students records
-                $sample_id = $this->db->insert_id();
-                //load file
-                $excel_file = $path.'/sps/assets/samples/'.$fileName;
-                $objPHPExcel = PHPExcel_IOFactory::load($excel_file);
-                //iterate all sheets and save data for each sheet
-                $num_sheets = $objPHPExcel->getSheetCount();
+          $path = $_SERVER['DOCUMENT_ROOT'];
+          $fileName = $sample_name.'_'.$file_name;
+          if(move_uploaded_file($file, $path.'/sps/assets/samples/'.$fileName)){
+            //store in database
+            $sample = array();
+            $sample['sample_name'] = $_POST['record_file'];
+            $sample['sample_file'] = $fileName;
+            $sample['author'] = $this->session->userdata['userid'];
+            $this->db->insert("sample_data", $sample);
+            //store students records
+            $sample_id = $this->db->insert_id();
+            //load file
+            $excel_file = $path.'/sps/assets/samples/'.$fileName;
+            $objPHPExcel = PHPExcel_IOFactory::load($excel_file);
+            //iterate all sheets and save data for each sheet
+            $num_sheets = $objPHPExcel->getSheetCount();
+            $row = 4;
+            for($num = 0; $num < $num_sheets; $num++){
+              $student = $objPHPExcel->getSheet($num)->getCell('A'.$row)->getValue();
+              $sex = $objPHPExcel->getSheet($num)->getCell('B'.$row)->getValue();
+              $regno = $objPHPExcel->getSheet($num)->getCell('C'.$row)->getValue();
+              $class = $objPHPExcel->getSheet($num)->getCell('A1')->getValue()->__toString();
+              //insert results
+              //term 1
+              $t1_bot_mtc = $objPHPExcel->getSheet($num)->getCell('D'.$row)->getValue();
+              $t1_bot_eng = $objPHPExcel->getSheet($num)->getCell('E'.$row)->getValue();
+              $t1_bot_sci = $objPHPExcel->getSheet($num)->getCell('F'.$row)->getValue();
+              $t1_bot_sst = $objPHPExcel->getSheet($num)->getCell('G'.$row)->getValue();
+              //mot
+              $t1_mot_mtc = $objPHPExcel->getSheet($num)->getCell('H'.$row)->getValue();
+              $t1_mot_eng = $objPHPExcel->getSheet($num)->getCell('I'.$row)->getValue();
+              $t1_mot_sci = $objPHPExcel->getSheet($num)->getCell('J'.$row)->getValue();
+              $t1_mot_sst = $objPHPExcel->getSheet($num)->getCell('K'.$row)->getValue();
+              //eot
+              $t1_eot_mtc = $objPHPExcel->getSheet($num)->getCell('L'.$row)->getValue();
+              $t1_eot_eng = $objPHPExcel->getSheet($num)->getCell('M'.$row)->getValue();
+              $t1_eot_sci = $objPHPExcel->getSheet($num)->getCell('N'.$row)->getValue();
+              $t1_eot_sst = $objPHPExcel->getSheet($num)->getCell('O'.$row)->getValue();
+              //Term 2
+              $t2_bot_mtc = $objPHPExcel->getSheet($num)->getCell('Q'.$row)->getValue();
+              $t2_bot_eng = $objPHPExcel->getSheet($num)->getCell('R'.$row)->getValue();
+              $t2_bot_sci = $objPHPExcel->getSheet($num)->getCell('S'.$row)->getValue();
+              $t2_bot_sst = $objPHPExcel->getSheet($num)->getCell('T'.$row)->getValue();
+              //mot
+              $t2_mot_mtc = $objPHPExcel->getSheet($num)->getCell('U'.$row)->getValue();
+              $t2_mot_eng = $objPHPExcel->getSheet($num)->getCell('V'.$row)->getValue();
+              $t2_mot_sci = $objPHPExcel->getSheet($num)->getCell('W'.$row)->getValue();
+              $t2_mot_sst = $objPHPExcel->getSheet($num)->getCell('X'.$row)->getValue();
+              //eot
+              $t2_eot_mtc = $objPHPExcel->getSheet($num)->getCell('Y'.$row)->getValue();
+              $t2_eot_eng = $objPHPExcel->getSheet($num)->getCell('Z'.$row)->getValue();
+              $t2_eot_sci = $objPHPExcel->getSheet($num)->getCell('AA'.$row)->getValue();
+              $t2_eot_sst = $objPHPExcel->getSheet($num)->getCell('AB'.$row)->getValue();
+              //Term 3
+              $t3_bot_mtc = $objPHPExcel->getSheet($num)->getCell('AD'.$row)->getValue();
+              $t3_bot_eng = $objPHPExcel->getSheet($num)->getCell('AE'.$row)->getValue();
+              $t3_bot_sci = $objPHPExcel->getSheet($num)->getCell('AF'.$row)->getValue();
+              $t3_bot_sst = $objPHPExcel->getSheet($num)->getCell('AG'.$row)->getValue();
+              //mot
+              $t3_mot_mtc = $objPHPExcel->getSheet($num)->getCell('AH'.$row)->getValue();
+              $t3_mot_eng = $objPHPExcel->getSheet($num)->getCell('AI'.$row)->getValue();
+              $t3_mot_sci = $objPHPExcel->getSheet($num)->getCell('AJ'.$row)->getValue();
+              $t3_mot_sst = $objPHPExcel->getSheet($num)->getCell('AK'.$row)->getValue();
+              //eot
+              $t3_eot_mtc = $objPHPExcel->getSheet($num)->getCell('AL'.$row)->getValue();
+              $t3_eot_eng = $objPHPExcel->getSheet($num)->getCell('AM'.$row)->getValue();
+              $t3_eot_sci = $objPHPExcel->getSheet($num)->getCell('AN'.$row)->getValue();
+              $t3_eot_sst = $objPHPExcel->getSheet($num)->getCell('AO'.$row)->getValue();
+              //insert into database
+              $this->db->insert("bulk_data", array(
+                "student"=> $student,
+                "sex"=> $sex,
+                "regno"=> $regno,
+                "class"=> $class,
+                "t1_bot_mtc"=> $t1_bot_mtc,
+                "t1_bot_eng"=> $t1_bot_eng,
+                "t1_bot_sci"=> $t1_bot_sci,
+                "t1_bot_sst"=> $t1_bot_sst,
+                "t1_mot_mtc"=> $t1_mot_mtc,
+                "t1_mot_eng"=> $t1_mot_eng,
+                "t1_mot_sci"=> $t1_mot_sci,
+                "t1_mot_sst"=> $t1_mot_sst,
+                "t1_eot_mtc"=> $t1_eot_mtc,
+                "t1_eot_eng"=> $t1_eot_eng,
+                "t1_eot_sci"=> $t1_eot_sci,
+                "t1_eot_sst"=> $t1_eot_sst,
+                "t2_bot_mtc"=> $t2_bot_mtc,
+                "t2_bot_eng"=> $t2_bot_eng,
+                "t2_bot_sci"=> $t2_bot_sci,
+                "t2_bot_sst"=> $t2_bot_sst,
+                "t2_mot_mtc"=> $t2_mot_mtc,
+                "t2_mot_eng"=> $t2_mot_eng,
+                "t2_mot_sci"=> $t2_mot_sci,
+                "t2_mot_sst"=> $t2_mot_sst,
+                "t2_eot_mtc"=> $t2_eot_mtc,
+                "t2_eot_eng"=> $t2_eot_eng,
+                "t2_eot_sci"=> $t2_eot_sci,
+                "t2_eot_sst"=> $t2_eot_sst,
+                "t3_bot_mtc"=> $t3_bot_mtc,
+                "t3_bot_eng"=> $t3_bot_eng,
+                "t3_bot_sci"=> $t3_bot_sci,
+                "t3_bot_sst"=> $t3_bot_sst,
+                "t3_mot_mtc"=> $t3_mot_mtc,
+                "t3_mot_eng"=> $t3_mot_eng,
+                "t3_mot_sci"=> $t3_mot_sci,
+                "t3_mot_sst"=> $t3_mot_sst,
+                "t3_eot_mtc"=> $t3_bot_mtc,
+                "t3_eot_eng"=> $t3_bot_eng,
+                "t3_eot_sci"=> $t3_bot_sci,
+                "t3_eot_sst"=> $t3_bot_sst
+              ));
 
-                for($num = 0; $num < $num_sheets; $num++){
-                  $class_fields = array();
 
-                  $class = $objPHPExcel->getSheet($num)->getCell('A1')->getValue()->__toString();
-                  //store class
-                  $class_fields['class'] = $class;
-                  $class_fields['author'] = $this->session->userdata['userid'];
-                  $this->db->insert("class", $class_fields);
-                  $class_id = $this->db->insert_id();
-                  //term
-                  $term_1 = $objPHPExcel->getSheet($num)->getCell('H1')->getValue();
-                  $term_2 = $objPHPExcel->getSheet($num)->getCell('U1')->getValue();
-                  $term_3 = $objPHPExcel->getSheet($num)->getCell('AH1')->getValue();
-                  //insert terms
-                  $terms = array(
-                    array(
-                      "term"=>$term_1,
-                      "class_id"=> $class_id,
-                      "author"=> $this->session->userdata['userid']
-                    ),
-                    array(
-                      "term"=>$term_2,
-                      "class_id"=> $class_id,
-                      "author"=> $this->session->userdata['userid']
-                    ),
-                    array(
-                      "term"=>$term_3,
-                      "class_id"=> $class_id,
-                      "author"=> $this->session->userdata['userid']
-                    )
-                  );
-                  $terms_inserted = count($terms);
-                  $this->db->insert_batch("terms", $terms);
-                  $first_term_db_entry = $this->db->insert_id();
-                  //term ids
-                  $term_1_id = $first_term_db_entry;
-                  $term_2_id = $term_1_id + 1;
-                  $term_3_id = $term_2_id + 1;
-                  //exam typess
-                 $exam_types = array();
-                 $exam_types['bot_t_1_1'] = $objPHPExcel->getSheet($num)->getCell('D2')->getValue();
-                 $exam_types['mot_t_1_2'] = $objPHPExcel->getSheet($num)->getCell('H2')->getValue();
-                 $exam_types['eot_t_1_3'] = $objPHPExcel->getSheet($num)->getCell('L2')->getValue();
-                 $exam_types['bot_t_2_1'] = $objPHPExcel->getSheet($num)->getCell('Q2')->getValue();
-                 $exam_types['mot_t_2_2'] = $objPHPExcel->getSheet($num)->getCell('U2')->getValue();
-                 $exam_types['eot_t_2_3'] = $objPHPExcel->getSheet($num)->getCell('Y2')->getValue();
-                 $exam_types['bot_t_3_1'] = $objPHPExcel->getSheet($num)->getCell('AD2')->getValue();
-                 $exam_types['mot_t_3_2'] = $objPHPExcel->getSheet($num)->getCell('AH2')->getValue();
-                 $exam_types['eot_t_3_3'] = $objPHPExcel->getSheet($num)->getCell('AL2')->getValue();
-                 $types_count = count($exam_types);
-
-                 $this->db->insert_batch("exam_type", array(
-                 array("term_id"=> $term_1_id, "type"=>  $exam_types['bot_t_1_1'], "class_id"=> $class_id, "author"=> $this->session->userdata['userid']),
-                 array("term_id"=> $term_1_id, "type"=>  $exam_types['mot_t_1_2'], "class_id"=> $class_id, "author"=> $this->session->userdata['userid']),
-                 array("term_id"=> $term_1_id, "type"=>  $exam_types['eot_t_1_3'], "class_id"=> $class_id, "author"=> $this->session->userdata['userid']),
-                 array("term_id"=> $term_2_id, "type"=>  $exam_types['bot_t_2_1'], "class_id"=> $class_id, "author"=> $this->session->userdata['userid']),
-                 array("term_id"=> $term_2_id, "type"=>  $exam_types['mot_t_2_2'], "class_id"=> $class_id, "author"=> $this->session->userdata['userid']),
-                 array("term_id"=> $term_2_id, "type"=>  $exam_types['eot_t_2_3'], "class_id"=> $class_id, "author"=> $this->session->userdata['userid']),
-                 array("term_id"=> $term_3_id, "type"=>  $exam_types['bot_t_3_1'], "class_id"=> $class_id, "author"=> $this->session->userdata['userid']),
-                 array("term_id"=> $term_3_id, "type"=>  $exam_types['mot_t_3_2'], "class_id"=> $class_id, "author"=> $this->session->userdata['userid']),
-                 array("term_id"=> $term_3_id, "type"=>  $exam_types['eot_t_3_3'], "class_id"=> $class_id, "author"=> $this->session->userdata['userid']),
-                 ));
-                 $exam_type_id = $this->db->insert_id();
-
-                 //students extract
-                 $students = 39;
-                 $row = 4;
-                 for($stud = 0; $stud <= $students; $stud++){                    
-                    $student_name = $objPHPExcel->getSheet($num)->getCell('A'.$row)->getValue();
-                    $student_sex = $objPHPExcel->getSheet($num)->getCell('B'.$row)->getValue();
-                    $student_reg = $objPHPExcel->getSheet($num)->getCell('C'.$row)->getValue();
-                    //save student
-                    $this->db->insert('students', array(
-                      'student'=> $student_name,
-                      'sex'=> $student_sex,
-                      'reg_no'=> $student_reg,
-                      'class_id'=> $class_id,
-                      'author'=> $this->session->userdata['userid']
-                    ));
-                    $student_id = $this->db->insert_id();
-                    //get term and exam type id
-                    //subjects 
-                    //$mtc_bot_t1 = $objPHPExcel->getSheet($num)->getCell('D'.$stud)->getValue();
-
-                  //  for($type = 0; $type < $types_count; $type++){
-                  //   //insert students performance for each exam type with id
-                  //   $this->db->insert('results_tbl', array(
-                  //     'student_id'=> $student_id,
-                  //     'term_id'=> ,
-                  //     'exam_type_id'=> $exam_type_id,
-                  //     'mtc'=>,
-                  //     'eng',
-                  //     'sci',
-                  //     'sst',
-                  //     'author'
-                  //   ));
-                  //   $exam_type_id++;
-                  //  }
-                  $row++;
-                 }
-
-                }//exit();
-            }else{
-              $data['msg'] = 'Error!. File upload failed, please try again';
+              $row++;
             }
+            
+          }//move_uploaded_file
+            
         }
+        
         $this->load->view("portal/upload", $data);
     }
     function history(){
