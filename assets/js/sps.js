f3162e200f3ddec37fce9e0723655497a33e2547
conrad96 <conrad.mugisha@gmail.com>
2019-12-13 16:55:08 +0300

    removed setTimeout function

--- assets/js/sps.js
+++ assets/js/sps.js
@@ -1,20 +1,17 @@
 $(document).on('change', "#sample", function(){
     var newSample = $("#sample").val();
     var getBaseURL = $("#getBaseURL").val();
-
-    $("#resultsCanvas").html("<center style='padding-top: 8%;'><img src='"+getBaseURL+"assets/img/loading.gif' style='width: 10%;'/></center>");
+    
     //hide results canvas for 5sec
-    var delay = 5000;
-    setTimeout(function(){
-        $("#resultsCanvas").show();
-        //
-        $.ajax({
-            type: "POST",
-            url: getBaseURL + 'index.php/User/filter',
-            data: {'field': 'BD.sample_id', 'value': newSample},
-            success: function(data){
-                $("#resultsCanvas").html(data);
-            }
-        });
-    }, delay);
+    $.ajax({
+        type: "POST",
+        url: getBaseURL + 'index.php/User/filter',
+        data: {'field': 'BD.sample_id', 'value': newSample},
+        success: function(data){
+            $("#resultsCanvas").html(data);
+        },
+        beforeSend: function(){
+            $("#resultsCanvas").html("<center style='padding-top: 8%;'><img src='"+getBaseURL+"assets/img/loading.gif' style='width: 10%;'/></center>");
+        }
+    });
 });
