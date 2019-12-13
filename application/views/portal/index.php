bf382ec59cff3536649e6d40d3193571821dd741
conrad96 <conrad.mugisha@gmail.com>
2019-12-13 13:40:40 +0300

    added filter table selection fields

--- application/views/portal/index.php
+++ application/views/portal/index.php
@@ -64,7 +64,29 @@
         </div>
         <!-- datatable showing students -->
         <div class="row">
-          
+          <!-- results -->           
+          <div class="col-md-5">
+            <h3><label class="col-md-2" for="term">Term</label></h3>
+            <div class="col-md-3">
+              <select class="input-lgm-bot15 form-control" name="term" id="term">
+                <option disabled >-Select-</option>
+                <option selected>Term 1</option>
+                <option>Term 2</option>
+                <option>Term 3</option>
+              </select>
+            </div>
+          </div>
+          <div class="col-md-5">
+          <h3><label class="col-md-2" for="term">Exam</label></h3>
+          <div class="col-md-3">
+              <select class="input-lgm-bot15 form-control" name="term" id="term">
+                <option disabled >-Select-</option>
+                <option selected>BOT</option>
+                <option>MOT</option>
+                <option>EOT</option>
+              </select>
+            </div>
+          </div>
         </div>
         
       </section>
