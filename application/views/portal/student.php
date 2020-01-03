67eb1c3567acd01d0c197594147c13d27bcb3281
conrad96 <conrad.mugisha@gmail.com>
2020-01-03 15:01:12 +0300

    added prediction grade and termly subject marks, plus conditional statements to grade candidates performance

--- application/views/portal/student.php
+++ application/views/portal/student.php
@@ -49,7 +49,212 @@
                             <h4 class="modal-title">Prediction statistics</h4>
                           </div>
                           <div class="modal-body">
-                              
+                              <!-- use yearly data to compile statistics getting averages and present grading system -->                              
+                             <?php $get_entire_students_record = $this->db->query("SELECT * FROM bulk_data S WHERE S.id = '".$student_id."' ")->result_array(); ?>
+                             <?php if(!empty($get_entire_students_record)){
+                                      foreach($get_entire_students_record as $studn){
+                                        ?>
+                                        <div class="row">
+                                              <div class="col-md-4">
+                                                <h4>Term 1 Results: </h4>
+                                                <?php $term_1_mtc = $term_2_mtc = $term_3_mtc = $term_1_eng = $term_2_eng = $term_3_eng = $term_1_sci = $term_2_sci = $term_3_sci = $term_1_sst = $term_2_sst = ''; ?>
+                                                <ul class="list-group">
+                                                  <?php $term_1_mtc = round((($studn['t1_bot_mtc'] + $studn['t1_mot_mtc'] + $studn['t1_eot_mtc']) / 3), 2); 
+                                                        $term_2_mtc = round((($studn['t2_bot_mtc'] + $studn['t2_mot_mtc'] + $studn['t2_eot_mtc']) / 3), 2);
+                                                        $term_3_mtc = round((($studn['t3_bot_mtc'] + $studn['t3_mot_mtc'] + $studn['t3_eot_mtc']) / 3), 2);
+                                                        //eng
+                                                        $term_2_eng = round((($studn['t2_bot_eng'] + $studn['t2_mot_eng'] + $studn['t2_eot_eng']) / 3), 2);
+                                                        $term_1_eng = round((($studn['t1_bot_eng'] + $studn['t1_mot_eng'] + $studn['t1_eot_eng']) / 3), 2);
+                                                        $term_3_eng = round((($studn['t3_bot_eng'] + $studn['t3_mot_eng'] + $studn['t3_eot_eng']) / 3), 2);
+                                                        //sci
+                                                        $term_1_sci = round((($studn['t1_bot_sci'] + $studn['t1_mot_sci'] + $studn['t1_eot_sci']) / 3), 2);
+                                                        $term_2_sci = round((($studn['t2_bot_sci'] + $studn['t2_mot_sci'] + $studn['t2_eot_sci']) / 3), 2);
+                                                        $term_3_sci = round((($studn['t3_bot_sci'] + $studn['t3_mot_sci'] + $studn['t3_eot_sci']) / 3), 2);
+                                                        //sst
+                                                        $term_1_sst = round((($studn['t1_bot_sst'] + $studn['t1_mot_sst'] + $studn['t1_eot_sst']) / 3), 2);
+                                                        $term_2_sst = round((($studn['t2_bot_sst'] + $studn['t2_mot_sst'] + $studn['t2_eot_sst']) / 3), 2);
+                                                        $term_3_sst = round((($studn['t3_bot_sst'] + $studn['t3_mot_sst'] + $studn['t3_eot_sst']) / 3), 2); 
+
+                                                  ?>
+                                                  <li class="list-group-item">Math: <?php echo $term_1_mtc ; ?></li>
+                                                  <li class="list-group-item">English: <?php echo $term_1_eng; ?></li>
+                                                  <li class="list-group-item">Science: <?php echo $term_1_sci; ?></li>
+                                                  <li class="list-group-item">SST: <?php echo $term_1_sst; ?></li>
+                                                </ul>
+                                              </div>
+                                              <div class="col-md-4">
+                                                <h4>Term 2 Results: </h4>
+                                                <ul class="list-group">
+                                                <li class="list-group-item">Math: <?php echo $term_2_mtc; ?></li>
+                                                  <li class="list-group-item">English: <?php echo $term_2_eng; ?></li>
+                                                  <li class="list-group-item">Science: <?php echo $term_2_sci; ?></li>
+                                                  <li class="list-group-item">SST: <?php echo  $term_2_sst; ?></li>
+                                                </ul>
+                                              </div>
+                                              <div class="col-md-4">
+                                                <h4>Term 3 Results: </h4>
+                                                <ul class="list-group">
+                                                <li class="list-group-item">Math: <?php echo $term_3_mtc; ?></li>
+                                                  <li class="list-group-item">English: <?php echo $term_3_eng; ?></li>
+                                                  <li class="list-group-item">Science: <?php echo $term_3_sci; ?></li>
+                                                  <li class="list-group-item">SST: <?php echo $term_3_sst; ?></li>
+                                                </ul>
+                                              </div>                                
+                                        </div>
+                                        <div class="row">
+                                          <div class="col-md-6">
+                                              <h4>Predicted Results:</h4>
+                                              <ul class="list-group">
+                                                <li class="list-group-item">Math: <?php echo round((($term_1_mtc + $term_2_mtc + $term_3_mtc) / 3), 2); ?></li>
+                                                <li class="list-group-item">English: <?php echo round((($term_1_eng + $term_2_eng + $term_3_eng) / 3), 2); ?></li>
+                                                <li class="list-group-item">Science: <?php echo round((($term_1_sci + $term_2_sci + $term_3_sci) / 3), 2); ?></li>
+                                                <li class="list-group-item">SST: <?php echo round((($term_1_sst + $term_2_sst + $term_3_sst) / 3), 2); ?></li>
+                                              </ul>
+                                          </div>
+                                          <div class="col-md-6">
+                                            <h4>Grades:</h4>
+                                            <ul class="list-group">                                              
+                                              <?php //term => subjects 
+                                                $term_mtc_avg =  round((($term_1_mtc + $term_2_mtc + $term_3_mtc) / 3), 2);
+                                                $grades_mtc = $grades_eng = $grades_sci = $grades_sst = '';
+                                                if($term_mtc_avg >= 80){
+                                                  $grades_mtc = 1;
+                                                  echo '<li class="list-group-item">'.$grades_mtc.'</li>';
+                                                }else if($term_mtc_avg >= 75){
+                                                  $grades_mtc = 2;
+                                                  echo '<li class="list-group-item">'.$grades_mtc.'</li>';
+                                                }else if($term_mtc_avg >= 70){
+                                                  $grades_mtc = 3;
+                                                  echo '<li class="list-group-item">'.$grades_mtc.'</li>';
+                                                }else if($term_mtc_avg >= 60){
+                                                  $grades_mtc = 4;
+                                                  echo '<li class="list-group-item">'.$grades_mtc.'</li>';
+                                                }else if($term_mtc_avg >= 55){
+                                                  $grades_mtc = 5;
+                                                  echo '<li class="list-group-item">'.$grades_mtc.'</li>';
+                                                }else if($term_mtc_avg >= 50){
+                                                  $grades_mtc = 6;
+                                                  echo '<li class="list-group-item">'.$grades_mtc.'</li>';
+                                                }else if($term_mtc_avg >= 45){
+                                                  $grades_mtc = 7;
+                                                  echo '<li class="list-group-item">'.$grades_mtc.'</li>';
+                                                }else if($term_mtc_avg >= 40){
+                                                  $grades_mtc = 8;
+                                                  echo '<li class="list-group-item">'.$grades_mtc.'</li>';
+                                                }else if($term_mtc_avg < 40){
+                                                  $grades_mtc = 9;
+                                                  echo '<li class="list-group-item">'.$grades_mtc.'</li>';
+                                                }
+                                                //eng
+                                                $term_eng_avg = round((($term_1_eng + $term_2_eng + $term_3_eng) / 3), 2);
+                                                if($term_eng_avg >= 80){
+                                                  $grades_eng = 1;
+                                                  echo '<li class="list-group-item">'.$grades_eng.'</li>';
+                                                }else if($term_eng_avg >= 75){
+                                                  $grades_eng = 2;
+                                                  echo '<li class="list-group-item">'.$grades_eng.'</li>';
+                                                }else if($term_eng_avg >= 70){
+                                                  $grades_eng = 3;
+                                                  echo '<li class="list-group-item">'.$grades_eng.'</li>';
+                                                }else if($term_eng_avg >= 60){
+                                                  $grades_eng = 4;
+                                                  echo '<li class="list-group-item">'.$grades_eng.'</li>';
+                                                }else if($term_eng_avg >= 55){
+                                                  $grades_eng = 5;
+                                                  echo '<li class="list-group-item">'.$grades_eng.'</li>';
+                                                }else if($term_eng_avg >= 50){
+                                                  $grades_eng = 6;
+                                                  echo '<li class="list-group-item">'.$grades_eng.'</li>';
+                                                }else if($term_eng_avg >= 45){
+                                                  $grades_eng = 7;
+                                                  echo '<li class="list-group-item">'.$grades_eng.'</li>';
+                                                }else if($term_eng_avg >= 40){
+                                                  $grades_eng = 8;
+                                                  echo '<li class="list-group-item">'.$grades_eng.'</li>';
+                                                }else if($term_eng_avg < 40){
+                                                  $grades_eng = 9;
+                                                  echo '<li class="list-group-item">'.$grades_eng.'</li>';
+                                                }
+                                                //sci avg
+                                                $term_sci_avg = round((($term_1_sci + $term_2_sci + $term_3_sci) / 3), 2);
+                                                if($term_sci_avg >= 80){
+                                                  $grades_sci = 1;
+                                                  echo '<li class="list-group-item">'.$grades_sci.'</li>';
+                                                }else if($term_sci_avg >= 75){
+                                                  $grades_sci = 2;
+                                                  echo '<li class="list-group-item">'.$grades_sci.'</li>';
+                                                }else if($term_sci_avg >= 70){
+                                                  $grades_sci = 3;
+                                                  echo '<li class="list-group-item">'.$grades_sci.'</li>';
+                                                }else if($term_sci_avg >= 60){
+                                                  $grades_sci = 4;
+                                                  echo '<li class="list-group-item">'.$grades_sci.'</li>';
+                                                }else if($term_sci_avg >= 55){
+                                                  $grades_sci = 5;
+                                                  echo '<li class="list-group-item">'.$grades_sci.'</li>';
+                                                }else if($term_sci_avg >= 50){
+                                                  $grades_sci = 6;
+                                                  echo '<li class="list-group-item">'.$grades_sci.'</li>';
+                                                }else if($term_sci_avg >= 45){
+                                                  $grades_sci = 7;
+                                                  echo '<li class="list-group-item">'.$grades_sci.'</li>';
+                                                }else if($term_sci_avg >= 40){
+                                                  $grades_sci = 8;
+                                                  echo '<li class="list-group-item">'.$grades_sci.'</li>';
+                                                }else if($term_sci_avg < 40){
+                                                  $grades_sci = 9;
+                                                  echo '<li class="list-group-item">'.$grades_sci.'</li>';
+                                                }
+                                                //sst avg
+                                                $term_sst_avg = round((($term_1_sst + $term_2_sst + $term_3_sst) / 3), 2);
+                                                if($term_sst_avg >= 80){
+                                                  $grades_sst = 1;
+                                                  echo '<li class="list-group-item">'.$grades_sst.'</li>';
+                                                }else if($term_sst_avg >= 75){
+                                                  $grades_sst = 2;
+                                                  echo '<li class="list-group-item">'.$grades_sst.'</li>';
+                                                }else if($term_sst_avg >= 70){
+                                                  $grades_sst = 3;
+                                                  echo '<li class="list-group-item">'.$grades_sst.'</li>';
+                                                }else if($term_sst_avg >= 60){
+                                                  $grades_sst = 4;
+                                                  echo '<li class="list-group-item">'.$grades_sst.'</li>';
+                                                }else if($term_sst_avg >= 55){
+                                                  $grades_sst = 5;
+                                                  echo '<li class="list-group-item">'.$grades_sst.'</li>';
+                                                }else if($term_sst_avg >= 50){
+                                                  $grades_sst = 6;
+                                                  echo '<li class="list-group-item">'.$grades_sst.'</li>';
+                                                }else if($term_sst_avg >= 45){
+                                                  $grades_sst = 7;
+                                                  echo '<li class="list-group-item">'.$grades_sst.'</li>';
+                                                }else if($term_sst_avg >= 40){
+                                                  $grades_sst = 8;
+                                                  echo '<li class="list-group-item">'.$grades_sst.'</li>';
+                                                }else if($term_sst_avg < 40){
+                                                  $grades_sst = 9;
+                                                  echo '<li class="list-group-item">'.$grades_sst.'</li>';
+                                                }
+                                              ?>
+                                            </ul>
+                                          </div>
+                                          <div class="col-md-12">
+                                                <h4>PLE Grade Prediction: <?php $sum_grade = ($grades_mtc + $grades_eng + $grades_sci + $grades_sst); 
+                                                echo $sum_grade;
+                                                if($sum_grade <= 12){
+                                                  echo '&nbsp;&nbsp;&nbsp;Grade: <b>First Grade</b>';
+                                                }else if($sum_grade <= 24){
+                                                  echo '&nbsp;&nbsp;&nbsp;Grade: <b>Second Grade</b>';
+                                                }else if($sum_grade < 24){
+                                                  echo '&nbsp;&nbsp;&nbsp;Grade: <b>Third Grade</b>';
+                                                }
+                                                
+                                                ?></h4>
+                                          </div>
+                                        </div>
+                                        <?php 
+                                      }
+                             } ?>
                           </div>
                           <div class="modal-footer">
                             <button data-dismiss="modal" class="btn btn-default" type="button">Close</button>
